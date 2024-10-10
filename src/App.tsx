876fb141f46b1f3ebd534212bd356946ce2f9c2c
Tonny <anthonny14gabriell@gmail.com>
2024-10-10 13:58:05 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Sum two numbers</h1>
+        <div>
+          <input type="number" />
+          <span>+</span>
+          <input type="number" />
+        </div>
+        <button>Sum</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
